ab7ecde4a2057d88456fc1782fc19adc146605e0
sandeep-cs-dev <sandeeppatelsgsits@gmail.com>
2025-08-26 01:57:55 +0200

    removing unused code

--- url-shortner/app/services/short_url.py
+++ url-shortner/app/services/short_url.py
@@ -5,6 +5,9 @@ from app.util.num_to_short_id import base62_encode
 
 def generate_short_url():
     uniq_id: UniqIdDTO = generate_uniq_id()
+
+    if uniq_id.uniq_id >= 67**7 :
+        raise ValueError("Max id exhausted to keep unique url code to max 7 length")
     uid = uniq_id.uniq_id % (67 ** 7)
     short_id = base62_encode(uid)
     return short_id
